5dda9a25f48146898a693df5db74b9ad35d88ca8
israel2496 <israelgs83@gmail.com>
2018-03-18 18:50:23 -0500

    multiple changes to proyect

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+        header.nav
+            .container
+                .nav-left
+                    .nav-item
+                        strong Platzi Music
+                .nav-right.nav-menu
+
+    .hero-body
+        .container.has-text-centered
+            h1.title Platzi Music
+            h2.subtitle Busca canciones
+            pm-player
+</template>
+
+<script>
+
+import PmPlayer from '@/components/Player.vue'
+
+export default {
+  components: {
+    PmPlayer
+  }
+}
+</script>
